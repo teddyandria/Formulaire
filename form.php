322283a6824a957b4ba0efb8ead8a9b0d5f7eb70
teddy.andria <teddy.andriamam@gmail.com>
2022-10-09 23:51:05 +0200

    add securisation

--- form.php
+++ form.php
@@ -15,20 +15,20 @@ include 'thanks.php';
 <form  action="thanks.php"  method="post">
     <div>
       <label  for="nom">Nom :</label>
-      <input  type="text"  id="nom"  name="user_name">
+      <input  type="text"  id="nom"  name="user_name" required>
     </div>
     <div>
       <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
+        <input  type="email"  id="courriel"  name="user_email" required>
     </div>
 
     <div>
         <label for="phone">Enter your phone number:</label>
-        <input type="tel" id="phone" name="phone" pattern="[0-9]{3} - [0-9]{3} - [0-9]{4}">
+        <input type="tel" id="phone" name="phone" pattern="[0-9]{3} - [0-9]{3} - [0-9]{4}" required>
     </div>
     <div>
         <label for="animals">Choose an animal</label>
-        <select name="animals" id="animals" size="4" multiple>
+        <select name="animals" id="animals" size="4" multiple required>
             <option value="Chien">Chien</option>
             <option value="Cafard">Cafard</option>
             <option value="Moustique">Moustique</option>
@@ -37,7 +37,7 @@ include 'thanks.php';
     </div>
     <div>
       <label  for="message">Message :</label>
-      <textarea  id="message"  name="user_message"></textarea>
+      <textarea  id="message"  name="user_message" required></textarea>
     </div>
    
     <div  class="button">
--- thanks.php
+++ thanks.php
@@ -1,6 +1,32 @@
 <?php 
-echo 'Merci ' . $_POST['user_name'] . ' de nous avoir contacté à propos de ' . $_POST['animals'] . '.' . PHP_EOL;
 
-echo "Un de nos conseillers vous contactera soit à l'adresse " . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['phone'] . ' dans les plus bref délais pour traiter votre demain :' . PHP_EOL;
-echo $_POST['user_message'];
+$errors = [];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $contact = array_map('trim', $_POST);
+
+       if (empty($contact['user_name'])){
+        $errors[] = 'Veuillez indiquer votre nom.';
+       } 
+       if (empty($contact['user_email'])){
+        $errors[] = 'Veuillez indiquer de email.';
+       }
+       if (!filter_var($contact['user_email'], FILTER_VALIDATE_EMAIL)){
+        $errors [] = 'Le format d\'email est incorrecte';
+       }
+       if (empty($contact['phone'])){
+        $errors[] = 'Veuillez indiquer votre numéro de téléphone.';
+       }
+       if (empty($contact['animals'])){
+        $errors[] = 'Veuillez choisir un animal';
+       }
+       if (empty($errors)){
+        echo 'Merci ' . $_POST['user_name'] . ' de nous avoir contacté à propos de ' . $_POST['animals'] . '.' . PHP_EOL;
+        echo "Un de nos conseillers vous contactera soit à l'adresse " . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['phone'] . ' dans les plus bref délais pour traiter votre demain :' . PHP_EOL;
+        echo $_POST['user_message'];
+        
+       }
+    }
+
 ?>
